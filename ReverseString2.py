337162e47f4976491b847870efebb873b4756520
McStupid <116470175+McStupid@users.noreply.github.com>
2022-10-26 10:05:19 +1100

    Update ReverseString2.py

--- ReverseString2.py
+++ ReverseString2.py
@@ -3,9 +3,10 @@ import streamlit as st
 
 #create function to reverse string
 def reverse_string(string):
-    return string[::-2] #Reverse the sting
+    return string[::-n] #Reverse the sting
 
-a = st.text_input("Input String: ")#Allow the user to input their own string
+s = st.text_input("Input String: ")#Allow the user to input their own string
+n = st.text_input("input string licing amount: ")#Allow the user to input their own string
 
 #Print the reversed string
-st.write("The revsered text is:",reverse_string(a))
+st.write("The revsered text is:",reverse_string(s))

4ce88e0da97736404770bfc3023c46ba9bbf58b4
McStupid <116470175+McStupid@users.noreply.github.com>
2022-10-26 10:06:44 +1100

    Update ReverseString2.py

--- ReverseString2.py
+++ ReverseString2.py
@@ -6,7 +6,7 @@ def reverse_string(string):
     return string[::-n] #Reverse the sting
 
 s = st.text_input("Input String: ")#Allow the user to input their own string
-n = st.text_input("input string licing amount: ")#Allow the user to input their own string
+n = st.number_input("input string licing amount: ")#Allow the user to input their own string
 
 #Print the reversed string
 st.write("The revsered text is:",reverse_string(s))

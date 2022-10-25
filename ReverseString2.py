4226bd5f06b35c7bbbb8d7a4ab3162d80b636405
McStupid <116470175+McStupid@users.noreply.github.com>
2022-10-26 10:31:42 +1100

    Update ReverseString2.py

--- ReverseString2.py
+++ ReverseString2.py
@@ -1,13 +1,15 @@
 #import libaries
 import streamlit as st
 
+st.title('Reverse String')
+st.header('By Aiden Moses')
+
 #create function to reverse string
 def reverse_string(string):
     return string[::-1] #Reverse the sting
 
 s = st.text_input("Input String: ")#Allow the user to input their own string
 st.button('Reverse string',reverse_string)
-
 
 #Print the reversed string
 st.write("The revsered text is:",reverse_string(s))

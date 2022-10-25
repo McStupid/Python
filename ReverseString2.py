ad4a1465807c086cbec9ac274d8706e35962bcb0
McStupid <116470175+McStupid@users.noreply.github.com>
2022-10-26 10:38:43 +1100

    Update ReverseString2.py

--- ReverseString2.py
+++ ReverseString2.py
@@ -7,14 +7,14 @@ st.subheader('By Aiden Moses')
 #create function to reverse string
 def reverse_string(string):
     return string[::-1] #Reverse the sting
-
-s = st.text_input("Input String: ")#Allow the user to input their own string
-st.button('Reverse string',reverse_string)
-
 st.balloons()
 st.progress(10)
 with st.spinner('Wait for it...'):
     time.sleep(10)
+  
+s = st.text_input("Input String: ")#Allow the user to input their own string
+st.button('Reverse string',reverse_string)
+
     
 #Print the reversed string
 st.write("The revsered text is:",reverse_string(s))

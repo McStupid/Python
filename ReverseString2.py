db1e62cd571ffb01d40629f3f4726c342aaf9261
McStupid <116470175+McStupid@users.noreply.github.com>
2022-10-26 10:16:22 +1100

    Update ReverseString2.py

--- ReverseString2.py
+++ ReverseString2.py
@@ -6,9 +6,8 @@ def reverse_string(string):
     return string[::-1] #Reverse the sting
 
 s = st.text_input("Input String: ")#Allow the user to input their own string
-n = st.number_input("input string licing amount: ")#Allow the user to input their own string
+n = st.number_input("How many times to print string: ")#Allow the user to input their own string
 
 
 #Print the reversed string
-st.write("The revsered text is:",reverse_string(s))
-st.color_picker('Pick a color',(s))
+st.write("The revsered text is:",reverse_string(s*n))

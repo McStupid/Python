0da9b84496d56b40a28cbea019510d6839ccd5a1
McStupid <116470175+McStupid@users.noreply.github.com>
2022-10-26 17:27:26 +1100

    Update ReverseString2.py

--- ReverseString2.py
+++ ReverseString2.py
@@ -11,11 +11,14 @@ def reverse_string(string):
     return string[::-1] #Reverse the sting by using string slice and reversing the output of he string
 
 s = st.text_input("Input String: ")#Allow the user to input their own string
+
 test_str = s
+
 Strlen = len([ele for ele in test_str if ele.isalpha()])
- 
-# printing result
-print("Count of Alphabets : " + str(Strlen))
+
+#test string length
+def test_string_count:
+  assert Strlen == reverse_string(s)
 
 #Print the reversed string
 btn = st.write("The reversed text is:",reverse_string(s))

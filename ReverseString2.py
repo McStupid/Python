51f3ab33f6924f0336db8a900f40531be680eccd
McStupid <116470175+McStupid@users.noreply.github.com>
2022-10-26 17:38:49 +1100

    Update ReverseString2.py

--- ReverseString2.py
+++ ReverseString2.py
@@ -1,29 +1,21 @@
-#import libaries
-import streamlit as st
-
-#Title headers and images 
-st.image('UC Logo.jpeg')
-st.title('Reverse String')
-st.subheader('By Aiden Moses')
-
-#create function to reverse string
-def reverse_string(string):
-    return string[::-1] #Reverse the sting by using string slice and reversing the output of he string
-
-s = st.text_input("Input String: ")#Allow the user to input their own string
-
-test_str = s
-
-Strlen = len([ele for ele in test_str if ele.isalpha()])
-
-#test string length
-def test_string_count():
-  assert Strlen == reverse_string(s)
-
-#Print the reversed string
-btn = st.write("The reversed text is:",reverse_string(s))
-
-st.button('Reverse string',btn) #button code that allos the user to presses and reverse string
+def main():
+    import streamlit as st
+    #Title headers and images 
+    st.image('UC Logo.jpeg')
+    st.title('Reverse String')
+    st.subheader('By Aiden Moses')
+    #create function to reverse string
+    def reverse_string(string):
+        return string[::-1] #Reverse the sting by using string slice and reversing the output of he string
+    s = st.text_input("Input String: ")#Allow the user to input their own string
+    test_str = s
+    Strlen = len([ele for ele in test_str if ele.isalpha()])
+    #test string length
+    def test_string_count():
+        assert Strlen == reverse_string(s)
+       #Print the reversed strin
+    btn = st.write("The reversed text is:",reverse_string(s))
+    st.button('Reverse string',btn) #button code that allos the user to presses and reverse string
 
     
 
